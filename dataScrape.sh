83550ceec48d719e3f5fee8ad05cd0e4ad19a195
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-10-23 23:29:13 +0200

    APIs and parsing added

--- dataScrape.sh
+++ dataScrape.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl https://www.coingecko.com/ | xmllint --html --xpath "/html/body/div[4]/div[4]/div[6]/div[1]/div/table/tbody/tr[1]/td[4]/div/div[2]/span/text()" - >> test.txt
+
+gecko="$(curl https://www.coingecko.com/ | xmllint --html --xpath '/html/body/div[4]/div[4]/div[6]/div[1]/div/table/tbody/tr[1]/td[4]/div/div[2]/span/text()' - | sed 's/[,\$]//g')"
+nomics="$(curl https://nomics.com/ | xmllint --html --xpath '//*[@id="__next"]/div[1]/div[4]/div[1]/div/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/div/span[1]/text()' - | sed 's/[,\$]//g')"
+cmc="$(curl https://coinmarketcap.com/ | xmllint --html --xpath '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a/span/text()' - | sed 's/[,\$]//g')"
+
+echo $(($gecko+$nomics+$cmc)) >> test.txt 
