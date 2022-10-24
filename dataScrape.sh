777f8d3f6febecc43e1c0fa8beb9ea127956231a
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-10-25 01:00:36 +0200

    testing to sort the result, not happy with the formatting on the iteration. Head < / 3

--- dataScrape.sh
+++ dataScrape.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+
 for i in `seq 1 60`;
 
-do
+	do
 
-gecko="$(curl https://www.coingecko.com/ | xmllint --html --xpath '/html/body/div[4]/div[4]/div[6]/div[1]/div/table/tbody/tr[1]/td[4]/div/div[2]/span/text()' - | sed 's/[,\$]//g')"
-nomics="$(curl https://nomics.com/ | xmllint --html --xpath '//*[@id="__next"]/div[1]/div[4]/div[1]/div/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/div/span[1]/text()' - | sed 's/[,\$]//g')"
-cmc="$(curl https://coinmarketcap.com/ | xmllint --html --xpath '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a/span/text()' - | sed 's/[,\$]//g')"
+		gecko="$(curl https://www.coingecko.com/ | xmllint --html --xpath '/html/body/div[4]/div[4]/div[6]/div[1]/div/table/tbody/tr[1]/td[4]/div/div[2]/span/text()' - | sed 's/[,\$]//g')"
+		nomics="$(curl https://nomics.com/ | xmllint --html --xpath '//*[@id="__next"]/div[1]/div[4]/div[1]/div/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/div/span[1]/text()' - | sed 's/[,\$]//g')"
+		cmc="$(curl https://coinmarketcap.com/ | xmllint --html --xpath '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a/span/text()' - | sed 's/[,\$]//g')"
 
-addedPrice=$(echo "$gecko + $nomics + $cmc" | bc )
+		addedPrice=$(echo "$gecko + $nomics + $cmc" | bc )
 
-btcPrice=$(echo "scale=4; $addedPrice / 3" | bc )
+		btcPrice=$(echo "scale=4; $addedPrice / 3" | bc )
+		
+		echo "$btcPrice" >> test2.txt
 
-echo "$i=$btcPrice" >> test.txt
+		sleep 1m 
 
-sleep 1m 
+	done
 
-done    
+ar="$(cat test2.txt )"
+IFS=$'\n'
+max="$(echo "${ar[*]}" | sort -nr | head -n1)"
+
+echo $max
+
+echo $max >> test2.txt
