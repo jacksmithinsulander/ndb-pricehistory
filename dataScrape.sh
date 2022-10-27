3409e409a92bf4b78b7e757bfcfb84c964445e74
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-10-27 18:44:18 +0200

    4h candle parsing done, will probably add some kind of dynamically set variables for urls to scrape

--- dataScrape.sh
+++ dataScrape.sh
@@ -1,38 +1,50 @@
 #!/bin/bash
 
-for i in `seq 1 5`;
+for j in {1..6}
 
 	do
 
-		gecko="$(curl https://www.coingecko.com/ | xmllint --html --xpath '/html/body/div[4]/div[4]/div[6]/div[1]/div/table/tbody/tr[1]/td[4]/div/div[2]/span/text()' - | sed 's/[,\$]//g')"
-		nomics="$(curl https://nomics.com/ | xmllint --html --xpath '//*[@id="__next"]/div[1]/div[4]/div[1]/div/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/div/span[1]/text()' - | sed 's/[,\$]//g')"
-		cmc="$(curl https://coinmarketcap.com/ | xmllint --html --xpath '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a/span/text()' - | sed 's/[,\$]//g')"
+		for i in `seq 1 240`;
 
-		addedPrice=$(echo "$gecko + $nomics + $cmc" | bc )
+			do
 
-		btcPrice=$(echo "scale=4; $addedPrice / 3" | bc )
+			gecko="$(curl https://www.coingecko.com/ | xmllint --html --xpath '/html/body/div[4]/div[4]/div[6]/div[1]/div/table/tbody/tr[1]/td[4]/div/div[2]/span/text()' - | sed 's/[,\$]//g')"
+			nomics="$(curl https://nomics.com/ | xmllint --html --xpath '//*[@id="__next"]/div[1]/div[4]/div[1]/div/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/div/span[1]/text()' - | sed 's/[,\$]//g')"
+			cmc="$(curl https://coinmarketcap.com/ | xmllint --html --xpath '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a/span/text()' - | sed 's/[,\$]//g')"
+
+			addedPrice=$(echo "$gecko + $nomics + $cmc" | bc )
+
+			btcPrice=$(echo "scale=4; $addedPrice / 3" | bc )
 		
-		echo "$btcPrice" >> test.txt
+			echo "$btcPrice" >> test.txt
 
-		sleep 1m 
+			sleep 1m 
 
-	done
+		done
 
-ar="$(cat test.txt )"
-IFS=$'\n'
-all="$(echo "${ar[*]}" | sort -nr)"
-max="$(echo "${ar[*]}" | sort -nr | head -n1)"
-min="$(echo "${ar[*]}" | sort -n | head -n1)"
-open="$(echo "${ar[@]}" | head -n1)"
-close="$(echo "${ar[*]}" | tail -n -1 )"
+	ar="$(cat test.txt )"
 
+	IFS=$'\n'
 
+	all="$(echo "${ar[*]}" | sort -nr)"
 
-echo $max
+	max="$(echo "${ar[*]}" | sort -nr | head -n1)"
 
-echo "all is $all" >> test2.txt
-echo "min is $min" >> test2.txt
-echo "max is $max" >> test2.txt
-echo "open is $open" >> test2.txt
-echo "close is $close" >> test2.txt
+	min="$(echo "${ar[*]}" | sort -n | head -n1)"
 
+	open="$(echo "${ar[@]}" | head -n1)"
+
+	close="$(echo "${ar[*]}" | tail -n -1 )"
+
+	date=$(date '+%Y%m%d')
+
+	echo "$date=\"$j\" 
+	Open=\"$open\"
+	Close=\"$close\"
+	Min=\"$min\"
+	Max=\"$max\"
+" >> price.db
+
+	rm -rf test.txt
+
+done
